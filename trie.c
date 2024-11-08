1bfd48590425f0f30874fe36517e86d580577df3
Aushiffer <caioshadow2014@gmail.com>
2024-11-08 12:19:49 -0300

    Implementa função de criação de nós

--- trie.c
+++ trie.c
@@ -2,6 +2,17 @@
 
 void inicia_trie(Trie *t) { *t = NULL; }
 
+Trie cria_no() {
+    Trie t;
+
+    if (!(t = (Trie)malloc(sizeof(No)))) return NULL;
+
+    for (int i = 0; i < NUM_DIG; i++)
+        t->vet_ap[i] = NULL; // Inicialização dos ponteiros.
+
+    return t;
+}
+
 unsigned char eh_folha(Trie t) {
     for (int i = 0; i < NUM_DIG; i++)
         if (t->vet_ap[i]) return 0;
