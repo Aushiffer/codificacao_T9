bef8d9e5325535e254f1cf0c7b0f287bd3029402
Aushiffer <caioshadow2014@gmail.com>
2024-11-09 12:22:13 -0300

    Implementa protótipos de inserção, casamento de padrões e modifica a estrutura da trie

--- trie.c
+++ trie.c
@@ -1,8 +1,10 @@
 #include "trie.h"
+#include <stdio.h>
+#include <string.h>
 
 void inicia_trie(Trie *t) { *t = NULL; }
 
-Trie cria_no() {
+Trie cria_no(char item) {
     Trie t;
 
     if (!(t = (Trie)malloc(sizeof(No)))) return NULL;
@@ -10,12 +12,135 @@ Trie cria_no() {
     for (int i = 0; i < NUM_DIG; i++)
         t->vet_ap[i] = NULL; // Inicialização dos ponteiros.
 
+    t->item = item; // Caractere pertencente ao caminho da palavra sendo inserida.
+
     return t;
 }
 
-unsigned char eh_folha(Trie t) {
-    for (int i = 0; i < NUM_DIG; i++)
-        if (t->vet_ap[i]) return 0;
+void busca_no(Trie t, char codigo[], char chave[]) {
+    char res[TAM_PALAVRA];
 
-    return 1;
+    busca_no_rec(t, codigo, chave, 0, res);
+    
+    if (strcmp(res, chave) == 0)
+        printf("%s\n", res);
+    else
+        printf("palavra nao encontrada\n");
 }
+
+void busca_no_rec(Trie t, char codigo[], char chave[], int d, char res[]) {
+    if (eh_folha(t) || codigo[d] == '\0') {
+        res[d] = '\0';
+
+        return;
+    }
+
+    Trie prox;
+    res[d] = t->item;
+
+    if (codigo[d] == '#')
+        prox = t->vet_ap[NUM_DIG - 1];
+    else
+        prox = t->vet_ap[codigo[d] - '2']; // f: [2..9] -> [0..8].
+
+    busca_no_rec(prox, codigo, chave, d + 1, res);
+}
+
+Trie insere_no(Trie t, char chave[], int d) {
+    if (!t) return cria_no(chave[d]);
+
+    if (d > 1 && chave[d] == chave[d - 1])
+        t->vet_ap[NUM_DIG - 1] = insere_no(t->vet_ap[NUM_DIG - 1], chave, d + 1);
+    else {
+        switch (padrao(chave[d])) {
+            case 2:
+                t->vet_ap[0] = insere_no(t->vet_ap[0], chave, d + 1);
+
+                break;
+            case 3:
+                t->vet_ap[1] = insere_no(t->vet_ap[1], chave, d + 1);
+
+                break;
+            case 4:
+                t->vet_ap[2] = insere_no(t->vet_ap[2], chave, d + 1);
+
+                break;
+            case 5:
+                t->vet_ap[3] = insere_no(t->vet_ap[3], chave, d + 1);
+
+                break;
+            case 6:
+                t->vet_ap[4] = insere_no(t->vet_ap[4], chave, d + 1);
+
+                break;
+            case 7:
+                t->vet_ap[5] = insere_no(t->vet_ap[5], chave, d + 1);
+
+                break;
+            case 8:
+                t->vet_ap[6] = insere_no(t->vet_ap[6], chave, d + 1);
+
+                break;
+            case 9:
+                t->vet_ap[7] = insere_no(t->vet_ap[7], chave, d + 1);
+
+                break;
+            default:
+                perror("padrao nao encontrado");
+
+                break;
+        }
+    }
+
+    return t;
+}
+
+int padrao(char c) {
+    if (
+        c == 'a'
+        || c == 'b'
+        || c == 'c'
+        || c == 'A'
+        || c == 'B'
+        || c == 'C'
+    )
+        return 2;
+
+    if (
+        c == 'd'
+        || c == 'e'
+        || c == 'f'
+        || c == 'D'
+        || c == 'E'
+        || c == 'F'
+    )
+        return 3;
+
+    if (
+        c == 'p' 
+        || c == 'q' 
+        || c == 'r' 
+        || c == 's'
+        || c == 'P'
+        || c == 'Q'
+        || c == 'R'
+        || c == 'S'
+    )
+        return 7;
+
+    if (
+        c == 'w'
+        || c == 'x'
+        || c == 'y'
+        || c == 'z'
+        || c == 'W'
+        || c == 'X'
+        || c == 'Y'
+        || c == 'Z'
+    )
+        return 9;
+
+    return -1;
+}
+
+unsigned char eh_folha(Trie t) { return t->item == '\0'; }
--- trie.h
+++ trie.h
@@ -1,28 +1,31 @@
 #ifndef __TRIE__
 #define __TRIE__
 
-// Número de dígitos possíveis + 1 caracter de terminação.
+// Número de dígitos possíveis.
 #define NUM_DIG 9
 
-// Padrões para cada dígito do teclado.
-#define PADRAO_2 "ABCabc"
-#define PADRAO_3 "DEFdef"
-#define PADRAO_4 "GHIghi"
-#define PADRAO_5 "JKLjkl"
-#define PADRAO_6 "MNOmno"
-#define PADRAO_7 "PQRSpqrs"
-#define PADRAO_8 "TUVtuv"
-#define PADRAO_9 "WXYZwxyz"
+// Tamanho máximo da palavra.
+#define TAM_PALAVRA 4096
+
+// Alfabeto de A a Z.
+#define ALFABETO "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+// Índices de padrões com quatro letras.
+#define INDICE_P 16
+#define INDICE_W 23
 
 // Dígitos possíveis
-#define ALFABETO "#23456789"
+#define DIGS_POSSIVEIS "23456789#"
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // TAD: Trie M-ária de existência.
 typedef struct No *Trie;
 typedef struct No {
     Trie vet_ap[NUM_DIG];
+    char item;
 } No;
 
 // Recebe um ponteiro para uma trie e o inicializa com NULL.
@@ -30,27 +33,26 @@ void inicia_trie(Trie *t);
 
 // Retorna um novo nó de uma trie existencial, cujos apontadores são nulos.
 // Caso a alocação gere erro, retorna NULL.
-Trie cria_no();
+Trie cria_no(char item);
 
 // Recebe uma trie, uma string e um valor posicional para o dígito sendo considerado;
 // Insere uma palavra na trie existencial, criando o caminho que representa a palavra.
-Trie insere_no(Trie t, char str[], int d);
+Trie insere_no(Trie t, char chave[], int d);
 
 // Recebe uma trie e um código em T9;
 // Realiza uma busca conforme a travessia na trie é realizada;
 // Se a string existir na trie, imprime a palavra buscada, caso contrário imprime 'palavra nao encontrada'.
 // É uma função auxiliar para a função recursiva de busca.
-void busca_no(Trie t, char codigo[]);
+void busca_no(Trie t, char codigo[], char chave[]);
 
 // Recebe uma trie, um código em T9 e um valor posicional para o dígito sendo considerado e uma string de resultado;
 // Busca uma palavra na trie existencial e coloca o resultado em uma string de consulta.
-void busca_no_rec(Trie t, char codigo[], int d, char res[]);
-
-// Recebe uma string de padrão, índices das extremidades do vetor 'l' e 'r' e um caracter 'c';
-// Retorna 1 se o caracter existe no padrão, 0 caso contrário.
-unsigned char existe(char padrao[], int l, int r, char c);
+void busca_no_rec(Trie t, char codigo[], char chave[], int d, char res[]);
 
 // Recebe uma trie e retorna 1 se o nó for uma folha, 0 caso contrário.
 unsigned char eh_folha(Trie t);
+
+// Recebe um caractere e retorna o padrão associado a ele. Caso o caractere não pertence a nenhum padrão, retorna -1.
+int padrao(char c);
 
 #endif // __TRIE__
